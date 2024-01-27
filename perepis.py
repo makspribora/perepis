e766ae6c5e94a4ca42c7a3c5f2c0faf21f99b512
makspribora <pribora2006@mail.ru>
2024-01-27 16:54:50 +0600

    list of surnames for first func

--- perepis.py
+++ perepis.py
@@ -1,11 +1,13 @@
 def count_older_than(year, data):
     count = 0
+    surnames = []
     for line in data:
         parts = line.split()
         birth_year = int(parts[-1].split('.')[2])
         if birth_year < year:
+            surnames.append(parts[0])
             count += 1
-    return count
+    return count, surnames
 
 def filter_by_year_range(start_year, end_year, data):
     result = []
@@ -20,8 +22,9 @@ try:
     with open("Perepis.txt", "r") as file:
         data = file.readlines()
 
-    older_than_1978 = count_older_than(1978, data)
+    older_than_1978, surnames = count_older_than(1978, data)
     print("Число жителей, родившихся до 1978 года:", older_than_1978)
+    print("Фамилии людей, родившихся до 1978 года:", ', '.join(surnames))
 
     start_year = int(input("Введите начальный год диапазона: "))
     end_year = int(input("Введите конечный год диапазона: "))
@@ -31,7 +34,7 @@ try:
     if (len(filtered_data)==0):
         print("Людей в этом диапозоне не найдено.")
     for person in filtered_data:
-        print(f"{person[0]} {person[1]} {person[2]} - {person[3]} год")
+        print(f"{person[0]} {person[1]} {person[2]} {person[3]} г. р.")
 
 except FileNotFoundError:
     print("Файл Perepis.txt не найден.")
