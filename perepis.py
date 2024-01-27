364799365b778508d8a1973e29ab89d16b554305
makspribora <pribora2006@mail.ru>
2024-01-27 16:07:38 +0600

    code

---
+++ perepis.py
@@ -0,0 +1,31 @@
+def count_older_than(year, data):
+    count = 0
+    for line in data:
+        parts = line.split()
+        birth_year = int(parts[-1].split('.')[2])
+        if birth_year < year:
+            count += 1
+    return count
+
+def filter_by_year_range(start_year, end_year, data):
+    result = []
+    for line in data:
+        parts = line.split()
+        birth_year = int(parts[-1].split('.')[2])
+        if start_year <= birth_year <= end_year:
+            result.append((parts[0], parts[1], parts[2], birth_year))
+    return result
+
+with open("Perepis.txt", "r") as file:
+    data = file.readlines()
+
+older_than_1978 = count_older_than(1978, data)
+print("Число жителей, родившихся до 1978 года:", older_than_1978)
+
+start_year = int(input("Введите начальный год диапазона: "))
+end_year = int(input("Введите конечный год диапазона: "))
+
+filtered_data = filter_by_year_range(start_year, end_year, data)
+print(f"Данные людей, родившихся с {start_year} по {end_year} год:")
+for person in filtered_data:
+    print(f"{person[0]} {person[1]} {person[2]} - {person[3]} год")
