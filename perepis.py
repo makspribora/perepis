8d983691281d058661bb3abd89a0e8f4a0f20183
makspribora <pribora2006@mail.ru>
2024-01-27 16:14:57 +0600

    exceptions

--- perepis.py
+++ perepis.py
@@ -16,16 +16,22 @@ def filter_by_year_range(start_year, end_year, data):
             result.append((parts[0], parts[1], parts[2], birth_year))
     return result
 
-with open("Perepis.txt", "r") as file:
-    data = file.readlines()
+try:
+    with open("Perepis.txt", "r") as file:
+        data = file.readlines()
 
-older_than_1978 = count_older_than(1978, data)
-print("Число жителей, родившихся до 1978 года:", older_than_1978)
+    older_than_1978 = count_older_than(1978, data)
+    print("Число жителей, родившихся до 1978 года:", older_than_1978)
 
-start_year = int(input("Введите начальный год диапазона: "))
-end_year = int(input("Введите конечный год диапазона: "))
+    start_year = int(input("Введите начальный год диапазона: "))
+    end_year = int(input("Введите конечный год диапазона: "))
 
-filtered_data = filter_by_year_range(start_year, end_year, data)
-print(f"Данные людей, родившихся с {start_year} по {end_year} год:")
-for person in filtered_data:
-    print(f"{person[0]} {person[1]} {person[2]} - {person[3]} год")
+    filtered_data = filter_by_year_range(start_year, end_year, data)
+    print(f"Данные людей, родившихся с {start_year} по {end_year} год:")
+    for person in filtered_data:
+        print(f"{person[0]} {person[1]} {person[2]} - {person[3]} год")
+
+except FileNotFoundError:
+    print("Файл Perepis.txt не найден.")
+except ValueError:
+    print("Ошибка ввода данных. Пожалуйста, введите года адекватно.")
